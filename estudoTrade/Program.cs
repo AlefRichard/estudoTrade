5005e404afd42a891f4b9fda7f025f8ec5cc6bbc
Alef Richard <alef.ferreira@rentcars.com>
2023-10-07 16:15:23 -0300

    Adicionar arquivos de projeto.

---
+++ estudoTrade/Program.cs
@@ -0,0 +1,42 @@
+﻿using Newtonsoft.Json;
+using System.Net;
+
+namespace estudoTrade
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Buscar dados
+            string[][] dados = GetDados("ETH_USDT", "DAY_1");
+
+            //Calculadora
+            int quantidade = dados.Count();
+
+            //Ação
+             //Comprar
+            Console.WriteLine(dados);
+
+        }
+
+        public static string[][] GetDados(string market, string interval)
+        {
+            WebRequest request = WebRequest.Create(string.Format(@"https://api.poloniex.com/markets/{0}/candles?interval={1}", market, interval));
+            WebResponse response = request.GetResponse();
+            Stream datastream = response.GetResponseStream();
+            StreamReader reader = new StreamReader(datastream);              
+            var content = reader.ReadToEnd();
+
+            string[][] data = JsonConvert.DeserializeObject<string[][]>(content);
+
+            if (data == null)
+                throw new Exception();
+            else
+                return data;
+
+
+
+        }
+
+    }
+}
